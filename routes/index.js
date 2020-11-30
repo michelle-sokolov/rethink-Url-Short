1f7cacaba144bcc96ac4de9687fcf2f4dacdd21a
michelle-sokolov <michellersokolov@gmail.com>
2020-11-29 17:54:15 -0800

    index ROUTE updated

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Url = require('../models/url');
 
+// route GET /:code
+// redirect to original URL 
+router.get('/:code', async (req, res) => {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+        return res.redirect(url.longUrl);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('server error')
+    }
+})
 module.exports = router;
